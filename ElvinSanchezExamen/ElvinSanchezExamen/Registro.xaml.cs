0b6b1ac5f532e7579e0e520e5b6eb103b7d4be60
Elvin Jesús Sánchez <laden1786@ymail.com>
2020-10-27 00:40:58 -0500

    Listo el Registro

--- ElvinSanchezExamen/ElvinSanchezExamen/Registro.xaml.cs
+++ ElvinSanchezExamen/ElvinSanchezExamen/Registro.xaml.cs
@@ -18,5 +18,73 @@ namespace ElvinSanchezExamen
             string usuario = lblUser.Text;
             lblUser.Text = usuario + user;
         }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double dato1 = Convert.ToDouble(txtDato1.Text);
+                if (dato1 < 1800)
+                {
+                    double saldo = 1800 - dato1;
+                    double cuota = saldo / 3;
+                    double parte = cuota + (cuota * 0.05);
+                    txtDato2.Text = parte.ToString();
+                    txtDato3.Text = parte.ToString();
+                    txtDato4.Text = parte.ToString();                
+                }
+                else
+                {
+                    if (dato1 == 1800)
+                    {
+                        double parte = 0;
+                        txtDato2.Text = parte.ToString();
+                        txtDato3.Text = parte.ToString();
+                        txtDato4.Text = parte.ToString();
+                        DisplayAlert("Notificación", "Curso Cancelado en su Totalidad", "Ok");
+                    }
+                    else
+                    {
+                        if (dato1 > 1800)
+                        {
+                            txtDato2.Text = "";
+                            txtDato3.Text = "";
+                            txtDato4.Text = "";
+                            DisplayAlert("Alerta", "Revise sus Valores Ingresados", "Reintentar");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", "ERROR" + ex.Message, "Ok.");
+            }
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == null)
+            {
+                DisplayAlert("Error", "Ingrese Datos", "Reintentar");
+            }
+            else
+            {
+                if (txtDato1.Text == null)
+                {
+                    DisplayAlert("Error", "Ingrese Datos", "Reintentar");
+                }
+                else
+                {
+                    DisplayAlert("Confirmación", "Elemento guardado con Exito", "Ok");
+                }
+            }
+        }
+
+        private async void btnResumen_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            await Navigation.PushAsync(new Resumen(nombre, apellido));
+        }
     }
 }
--- ElvinSanchezExamen/ElvinSanchezExamen/Resumen.xaml.cs
+++ ElvinSanchezExamen/ElvinSanchezExamen/Resumen.xaml.cs
@@ -12,9 +12,13 @@ namespace ElvinSanchezExamen
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Resumen : ContentPage
     {
-        public Resumen()
+        public Resumen(string nombre, string apellido)
         {
             InitializeComponent();
+            string name = lblNombre.Text;
+            lblNombre.Text = name + nombre;
+
+            
         }
     }
 }
